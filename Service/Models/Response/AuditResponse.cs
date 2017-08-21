e6c35861e12ac56b485927e0cf271279315523d9
Chris Recknell <Chris.Recknell@3chillies.co.uk>
2017-08-21 11:53:21 +0100

    Adding very basic audit stub to service

---
+++ Service/Models/Response/AuditResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Service.Models.Response
+{
+    public class AuditResponse
+    {
+        public bool AuditRequestResult { get; set; }
+    }
+}
